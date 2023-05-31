5b7001e3d9ab64591115b89de3b0ae9dd96603e2
Rhys <sorry12345677687757567@gmail.com>
2023-05-31 10:28:24 +0100

    smh mesa again

--- autoinstall.sh
+++ autoinstall.sh
@@ -4,12 +4,12 @@ set -x
 
 mkdir -p "$HOME/emudeck/mesa/22.2.1/"
 
-curl -l "https://raw.githubusercontent.com/piplup55/misc/yuzu/mesa/mesa/22.1.1/radeon_icd.x86_64.json" -o "$HOME/emudeck/mesa/22.2.1/radeon_icd.x86_64.json"
+curl -l "https://raw.githubusercontent.com/piplup55/misc/ryujinz/mesa/mesa/22.1.1/radeon_icd.x86_64.json" -o "$HOME/emudeck/mesa/22.2.1/radeon_icd.x86_64.json"
 curl -l "https://steamdeck-packages.steamos.cloud/archlinux-mirror/extra-main/os/x86_64/vulkan-radeon-22.2.1-1-x86_64.pkg.tar.zst" -o "$HOME/emudeck/mesa/22.2.1/radeon.tar.zst"
 tar --extract  -C "$HOME/emudeck/mesa/22.2.1/" --file="$HOME/emudeck/mesa/22.2.1/radeon.tar.zst" "usr/lib/libvulkan_radeon.so" --strip-components=2 && rm -f "$HOME/emudeck/mesa/22.2.1/radeon.tar.zst"
 
-if [ -e "$toolsPath\launchers\yuzu.sh" ]; then
-    mv "$toolsPath\launchers\yuzu.sh" "$toolsPath\launchers\yuzu.sh.bak"
+if [ -e "$toolsPath\launchers\ryujinx.sh" ]; then
+    mv "$toolsPath\launchers\ryujinx.sh" "$toolsPath\launchers\ryujinx.sh.bak"
 fi
 
-curl -l "https://raw.githubusercontent.com/piplup55/misc/yuzu/mesa/yuzu.sh" -o "$toolsPath\launchers\yuzu.sh" && chmod +x "$toolsPath\launchers\yuzu.sh"
+curl -l "https://raw.githubusercontent.com/piplup55/misc/ryujinx/mesa/ryujinx.sh" -o "$toolsPath\launchers\ryujinx.sh" && chmod +x "$toolsPath\launchers\ryujinx.sh"
